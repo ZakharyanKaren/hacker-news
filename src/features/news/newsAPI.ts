e43c66957bb5a113817f1385b7c42e5d1c71cc5a
ZakharyanKaren <Zakharyan.kar@gmail.com>
2022-12-02 11:12:52 +0400

    finished task

--- src/features/news/newsAPI.ts
+++ src/features/news/newsAPI.ts
@@ -1,4 +1,4 @@
-// const getNewsApi = "https://newsapi.org/v2/everything?domains=wsj.com&apiKey=bf3250e5fe4444cdaea8e021c2317fa4";
+const getNewsApi = "https://newsapi.org/v2/everything?domains=wsj.com&apiKey=bf3250e5fe4444cdaea8e021c2317fa4";
 
 export async function fetchNews() {
   return fetch(getNewsApi).then(res => {console.log(res); return res.json()});
